151ceaac7fb685d922566c5d881f56986564dc3a
Josh Milo <josh@joshmilo.com>
2013-04-11 13:11:57 -0700

    create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+confirm("Are you ready?");
+
+//var answer = Math.floor(Math.random()*11);
+
+var theNumber = Number(prompt("Pick a number", ""));
+    if (theNumber >= 6)
+        alert("too high");
+    if (theNumber <= 4)
+        alert("too low");
+    if (theNumber == 5)
+        alert("awesome");
